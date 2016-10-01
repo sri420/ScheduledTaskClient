32fd939fa761294992d24995480509345ddfb31f
Sridhar <Sridharb.in@gmail.com>
2016-10-01 15:49:45 +0530

    Added InitialCode

---
+++ src/main/java/com/demo/samples/DemoScheduledApp.java
@@ -0,0 +1,12 @@
+package com.demo.samples;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoScheduledApp {		
+	public static void main(String[] args) {
+		SpringApplication.run(DemoScheduledApp.class, args);
+	}
+}
